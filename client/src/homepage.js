0d7aa7b7d36ecac5adf42b556b588a6409d42304
EvanCh08 <evandarren04@gmail.com>
2024-05-15 09:54:52 +0800

    Update homepage.js

--- client/src/homepage.js
+++ client/src/homepage.js
@@ -113,10 +113,8 @@ export default function Home() {
                 : item
             )
           );
-          setAgentResponse(agentData.response); // Update the response in the data array
+          setAgentResponse(itemResponse); // Update the response in the data array
           setUserInput("");
-          setIsLoading(false) 
-          setDisplayInput(true)
         } catch (error) {
           console.error('Error processing data:', error);
           throw error; // Rethrow to ensure it's caught by .catch()
