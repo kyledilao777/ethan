d43cecd569b0c5283c9f3985ac4f422761b5eee1
EvanCh08 <evandarren04@gmail.com>
2024-07-14 17:06:02 +0800

    can hopefully

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3001;
 const clientId = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 const redirectUri =
-  /*process.env.GOOGLE_REDIRECT_URI || */"http://localhost:3001/oauth2callback";
+  process.env.GOOGLE_REDIRECT_URI /*|| "http://localhost:3001/oauth2callback"*/;
 const oAuth2Client = new google.auth.OAuth2(
   clientId,
   clientSecret,
