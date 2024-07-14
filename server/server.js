04c71b5c55f03ab3773dee3853495b56ff6f0fbd
EvanCh08 <evandarren04@gmail.com>
2024-07-14 16:56:54 +0800

    commit yay

--- server/server.js
+++ server/server.js
@@ -80,6 +80,7 @@ async function main() {
       secret: process.env.SESSION_SECRET,
       store: sessionStore,
       resave: false,
+      // saveUninitialized: true,
       saveUninitialized: false,
       cookie: {
         httpOnly: true,
