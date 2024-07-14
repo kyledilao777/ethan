980bf3ce240719549f3ba98cb80af181ac333b1f
EvanCh08 <evandarren04@gmail.com>
2024-07-14 17:31:04 +0800

    hopefully final

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -41,18 +41,20 @@ export default function NavBar({
     if (!confirmation) {
       return; // Abort logout if user cancels
     }
-
-    try {
-      axios.get("http://localhost:3001/logout", {
-        withCredentials: true,
+  
+    axios.get(process.env.REACT_LOGOUT_URL /*|| "http://localhost:3001/logout"*/, {
+      withCredentials: true,
+    })
+      .then(() => {
+        console.log("hello");
+        window.location.reload();
+        console.log("halo");
+      })
+      .catch((error) => {
+        console.error("Error logging out:", error);
       });
-      console.log("hello");
-      window.location.reload();
-      console.log("halo");
-    } catch (error) {
-      console.error("Error logging out:", error);
-    }
   };
+  
 
   useEffect(() => {
     const fetchUserInfo = async () => {
