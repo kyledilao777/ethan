cdb4b0881d9ca00064a4afa20f072b9443721cbc
EvanCh08 <evandarren04@gmail.com>
2024-07-21 13:59:26 +0800

    hellow there

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import Home from "./homepage";
 import Calendar from "./calendar";
@@ -10,13 +15,26 @@ import Documentation from "./documentation";
 import Privacy from "./privacy";
 import UserInfo from "./userinfo";
 import Terms from "./termsandservices";
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
-const TRACKING_ID = "G-5NYP0WVVQM"; // Replace with your tracking ID
+const TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID; // Replace with your tracking ID
 ReactGA.initialize(TRACKING_ID);
 
+function usePageViews() {
+  let history = useHistory();
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+    return history.listen((location) => {
+      ReactGA.pageview(location.pathname + location.search);
+    });
+  }, [history]);
+}
 
 export default function App() {
+  usePageViews();
+
   return (
     <div>
       <Router>
